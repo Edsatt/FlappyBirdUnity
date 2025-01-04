f7aa6b917016bb691c2953f56fccf3a7dee9c15e
Edward Satterthwaite <e.satterthwaite@outlook.com>
2025-01-04 13:40:09 +0000

    Incorporated sound effects into game

--- Assets/Code/Gameplay/Birb/BirbScript.cs
+++ Assets/Code/Gameplay/Birb/BirbScript.cs
@@ -5,6 +5,9 @@ namespace Code.Gameplay.Birb
 {
     public class BirbScript : CollisionObject
     {
+        private bool _isFirstFlap = true;
+        [SerializeField] private AudioClip flapSound1;
+        [SerializeField] private AudioClip flapSound2;
         public Rigidbody2D rigidBody;
         public float flapStrength;
         
@@ -13,6 +16,15 @@ namespace Code.Gameplay.Birb
             if (Input.GetKeyDown(KeyCode.Space) && !GameManager.Instance.IsGameOver)
             {
                 rigidBody.linearVelocity = Vector2.up * flapStrength;
+                if (_isFirstFlap)
+                {
+                    AudioManager.Instance.PlayOneShot(flapSound1);
+                    _isFirstFlap = false;
+                }
+                else
+                {
+                    AudioManager.Instance.PlayOneShot(flapSound2);
+                }
             }
         }
     }
--- Assets/Code/Managers/AudioManager.cs
+++ Assets/Code/Managers/AudioManager.cs
@@ -1,9 +1,12 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Code.Managers
 {
     public class AudioManager : MonoBehaviour
     {
+        public AudioSource audioSource;
+        
         private static AudioManager _instance;
 
         public static AudioManager Instance
@@ -21,6 +24,22 @@ namespace Code.Managers
         private void Awake()
         {
             _instance = this;
+        }
+
+        public void PlayOneShot(AudioClip clip)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+
+        public void PlayAudio([CanBeNull] AudioClip audioClip)
+        {
+            if (audioClip is null)
+            {
+                Debug.LogError("Audio clip is null!");
+                return;
+            }
+            audioSource.clip = audioClip;
+            audioSource.Play();
         }
     }
 }
